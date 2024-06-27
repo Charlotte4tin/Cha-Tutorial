06b930ebcbbc4181d9b61832c8b11b23735574ed
Charlotte <cefortin@me.com>
2024-06-27 10:50:09 -0400

    Initial Commit

---
+++ Cha Tutorial/Cha_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cha_TutorialApp.swift
+//  Cha Tutorial
+//
+//  Created by Charlotte Fortin on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Cha_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cha TutorialUITests/Cha_TutorialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cha_TutorialUITestsLaunchTests.swift
+//  Cha TutorialUITests
+//
+//  Created by Charlotte Fortin on 6/27/24.
+//
+
+import XCTest
+
+final class Cha_TutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
